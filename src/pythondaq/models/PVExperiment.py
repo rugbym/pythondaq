b511a1d87cedf6706d451de17dc60accd849ccfe
rugbym <94176296+rugbym@users.noreply.github.com>
2021-12-17 14:58:46 +0100

    fitting I_U graph, csv namecolumns, small error R

--- src/pythondaq/models/PVExperiment.py
+++ src/pythondaq/models/PVExperiment.py
@@ -1,6 +1,7 @@
 from os import write
 import csv
 import numpy as np
+from scipy.constants import Boltzmann, elementary_charge
 from statistics import mean, stdev
 from math import sqrt
 import threading
@@ -8,6 +9,8 @@ from pythondaq.controllers.arduino_device import ArduinoVISADevice as AVD
 from pythondaq.controllers.arduino_device import ConnectedDevs as CD
 import os
 import pandas as pd
+import lmfit
+from lmfit import models
 
 """Module to model data obtained using the arduino_device module from the controllers.
 """
@@ -29,6 +32,7 @@ class PVExperiment:
         self.U_zero_list = []
         self.R_MOSFET_list = []
         self.R_MOSFET_err_list = []
+        self.fit_plot_list = []
 
     def deviceinfo(self):
         """Info about the current connected device."""
@@ -97,7 +101,10 @@ class PVExperiment:
             err_pv_voltage = stdev(U) / sqrt(samplesize)
             err_pv_I = stdev(I) / sqrt(samplesize)
             err_pv_power = stdev(P) / sqrt(samplesize)
-            err_mosfet_R = stdev(R) / sqrt(samplesize)
+            try:
+                err_mosfet_R = stdev(R) / sqrt(samplesize)
+            except stdev(R) / sqrt(samplesize) == 0:
+                err_mosfet_R = 0.0001
         else:
             err_pv_voltage = float("nan")
             err_pv_I = float("nan")
@@ -121,6 +128,51 @@ class PVExperiment:
             target=self.scan, args=(nsteps, samplesize, begin, end)
         )
         self._scan_thread.start()
+
+    def fit_it(self):
+        self.fit_plot_list = []
+        fitfunc = lambda U, nT, I_l, I_0, e, k: I_l - I_0 * (
+            np.exp((e * U) / (nT * k)) - 1
+        )
+        model = models.Model(fitfunc)
+        params = model.make_params()
+        params.add("nT", value=5000)
+        params.add("e", value=elementary_charge, vary=False)
+        params.add("k", value=Boltzmann, vary=False)
+        params.add("I_l", value=0.02)
+        params.add("I_0", value=0.0)
+
+        self.fit = model.fit(
+            U=np.array(self.U_list),
+            data=np.array(self.I_list),
+            weights=1 / np.array(self.I_err_list),
+            params=params,
+        )
+
+        self.nT, self.nT_error = (
+            self.fit.params["nT"].value,
+            self.fit.params["nT"].stderr,
+        )
+        self.I_l, self.I_l_error = (
+            self.fit.params["I_l"].value,
+            self.fit.params["I_l"].stderr,
+        )
+        self.I_0, self.I_0_error = (
+            self.fit.params["I_0"].value,
+            self.fit.params["I_0"].stderr,
+        )
+        U_list_for_fit_plot = np.linspace(min(self.U_list), max(self.U_list), 100)
+        self.fit_plot_list = [
+            U_list_for_fit_plot,
+            fitfunc(
+                U_list_for_fit_plot,
+                self.nT,
+                self.I_l,
+                self.I_0,
+                elementary_charge,
+                Boltzmann,
+            ),
+        ]
 
     def scan(self, nsteps, samplesize, begin=0, end=3.3):
         """Perform measurements across a range of voltages.
@@ -163,6 +215,7 @@ class PVExperiment:
             self.U_zero_list.append(voltage)
             self.R_MOSFET_list.append(r)
             self.R_MOSFET_err_list.append(r_err)
+        self.reset_out()
         return self.scan_data
 
     def reset_out(self):
@@ -191,10 +244,14 @@ class PVExperiment:
             writer.writerow(
                 [
                     "Output voltage (V)",
-                    "Mean current (A)",
                     "Mean voltage (V)",
-                    "\u03B4 I (A)",
+                    "Mean current (A)",
+                    "Mean Power (W)",
+                    "R MOSFET (\u2126)",
                     "\u03B4 U (V)",
+                    "\u03B4 I (A)",
+                    "\u03B4 P (W)",
+                    "\u03B4 R (\u2126)",
                 ]
             )
             for measurement in self.scan_data:
